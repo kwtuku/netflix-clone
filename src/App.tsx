ac30dfe5689517859b1381dbe770021d7eba2f0f
kwtuku <76758720+kwtuku@users.noreply.github.com>
2021-08-27 22:50:33 +0900

    apiのデータを取得できるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Row } from './Row';
+import { requests } from "./request";
 
 function App() {
   return (
     <div>
       Hello world!
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="DOcumentaries" fetchUrl={requests.fetchDocumentMovies} />
     </div>
   );
 }
